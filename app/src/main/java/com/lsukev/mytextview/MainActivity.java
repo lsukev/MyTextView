6fc4a7f392ce7bc31146472d8df601d41a74c01b
Kevin Ivy <kevinivy1@gmail.com>
2016-08-23 16:59:09 -0500

    first push

---
+++ app/src/main/java/com/lsukev/mytextview/MainActivity.java
@@ -0,0 +1,16 @@
+package com.lsukev.mytextview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView myTextViewId = (TextView)findViewById(R.id.myTextViewId);
+    }
+}
