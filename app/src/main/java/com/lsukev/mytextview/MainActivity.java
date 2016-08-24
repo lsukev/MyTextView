d9c0d761e1cdd64c98838288675fab5a491cbd5b
Kevin Ivy <kevinivy1@gmail.com>
2016-08-23 22:02:08 -0500

    Finished the textview and button tutorial

--- app/src/main/java/com/lsukev/mytextview/MainActivity.java
+++ app/src/main/java/com/lsukev/mytextview/MainActivity.java
@@ -1,16 +1,33 @@
 package com.lsukev.mytextview;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonId;
+    private TextView textViewId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView myTextViewId = (TextView)findViewById(R.id.myTextViewId);
+        buttonId = (Button)findViewById(R.id.buttonId);
+        textViewId = (TextView)findViewById(R.id.textViewId);
+
+        buttonId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "I was clicked", Toast.LENGTH_LONG).show();
+                textViewId.setText("The button was pressed!");
+
+            }
+        });
     }
 }
